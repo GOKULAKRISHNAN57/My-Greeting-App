b5ded43526109f1b005d51c88a1e8acd68898900
GOKULAKRISHNAN57 <ramajayam2022@gmail.com>
2026-06-18 07:02:38 +0530

    UC2 - Greetings Completed

---
+++ My_Greetings_App/src/main/java/GreetingApplication/Service/GreetingService.java
@@ -0,0 +1,11 @@
+package GreetingApplication.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+
+    public String getGreetingMessage() {
+        return "Hello World";
+    }
+}
