5cc00392d85ccc65720f84b81ada8644b0a583e8
GOKULAKRISHNAN57 <ramajayam2022@gmail.com>
2026-06-18 06:56:16 +0530

    Added Controller

---
+++ My_Greetings_App/src/main/java/GreetingApplication/Controller/GreetingController.java
@@ -0,0 +1,18 @@
+package GreetingApplication.Controller;
+
+import com.bridgelabz.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @Autowired
+    private GreetingService greetingService;
+
+    @GetMapping("/greet")
+    public String greeting() {
+        return ""
+    }
+}
