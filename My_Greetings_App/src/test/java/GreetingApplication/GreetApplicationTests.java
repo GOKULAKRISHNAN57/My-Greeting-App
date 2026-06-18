cfb09afdafe15c60b1da0eb3cbac9176bc4f45e5
GOKULAKRISHNAN57 <ramajayam2022@gmail.com>
2026-06-18 06:50:37 +0530

    develop branch created

---
+++ My_Greetings_App/src/test/java/GreetingApplication/GreetApplicationTests.java
@@ -0,0 +1,13 @@
+package GreetingApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
