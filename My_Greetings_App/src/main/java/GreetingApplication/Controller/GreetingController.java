392ee0647bead991d028d08ca564f1d2ef4e4316
GOKULAKRISHNAN57 <ramajayam2022@gmail.com>
2026-06-18 07:08:20 +0530

    Greet Message Completed

--- My_Greetings_App/src/main/java/GreetingApplication/Controller/GreetingController.java
+++ My_Greetings_App/src/main/java/GreetingApplication/Controller/GreetingController.java
@@ -3,16 +3,23 @@ package GreetingApplication.Controller;
 import GreetingApplication.Service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/greeting")
 public class GreetingController {
 
-    @Autowired
     private GreetingService greetingService;
 
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+
     @GetMapping("/greet")
-    public String greeting() {
-        return greetingService.getGreetingMessage();
+    public String greeting(@RequestParam(required = false) String firstName, @RequestParam(required = false) String lastName) {
+        return greetingService.getGreetingMessage(firstName,lastName);
     }
 }
--- My_Greetings_App/src/main/java/GreetingApplication/Service/GreetingService.java
+++ My_Greetings_App/src/main/java/GreetingApplication/Service/GreetingService.java
@@ -5,7 +5,20 @@ import org.springframework.stereotype.Service;
 @Service
 public class GreetingService {
 
-    public String getGreetingMessage() {
-        return "Hello World";
+    public String getGreetingMessage(String firstName, String lastName) {
+
+        if(firstName != null && lastName != null) {
+            return "Hello " + firstName + " " + lastName;
+        }
+
+        if(firstName != null) {
+            return "Hello " + firstName;
+        }
+
+        if(lastName != null) {
+            return "Hello " + lastName;
+        }
+
+        return "Hello User";
     }
 }
